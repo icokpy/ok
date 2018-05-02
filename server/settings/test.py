748311b0e72bb53804a0b65a3bed000ef1cbf0c3
Navjot Kukreja <navjotk@gmail.com>
2018-05-02 11:01:59 +0100

    Pass SSL certificate to SQLAlchemy

--- server/settings/test.py
+++ server/settings/test.py
@@ -26,6 +26,11 @@ if SQLALCHEMY_DATABASE_URI.startswith(sqlite_prefix):
     SQLALCHEMY_DATABASE_URI = (sqlite_prefix +
             os.path.abspath(SQLALCHEMY_DATABASE_URI[len(sqlite_prefix) + 1:]))
 
+sql_ca_cert = os.getenv('SQL_CA_CERT')
+azure_uri = "azure"
+if sql_ca_cert and azure_uri in SQLALCHEMY_DATABASE_URI:
+    SQLALCHEMY_ENGINE_OPTS = {'connect_args': {'ssl': {'ca': sql_ca_cert}}}
+
 WTF_CSRF_CHECK_DEFAULT = False
 WTF_CSRF_ENABLED = False
 
